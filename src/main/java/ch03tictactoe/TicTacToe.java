345bfc4e414b8cf25d2148eeed5bfa7178f84ac1
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-07-18 00:03:02 -0300

    first commit

---
+++ src/main/java/ch03tictactoe/TicTacToe.java
@@ -0,0 +1,29 @@
+package ch03tictactoe;
+
+public class TicTacToe {
+    private Character[][] board = {
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'}
+    };
+
+    public void play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x, y);
+    }
+
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException("X is outside board");
+        }
+    }
+
+    private void setBox(int x, int y) {
+        if (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException("Box is occupied");
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
+    }
+}
